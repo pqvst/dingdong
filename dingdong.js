c77dc8d6aa30ada0db4c01b1b864d6a5b4e368e8
Philip Bergqvist <philip@unifiedintents.com>
2017-02-25 16:23:49 +0900

    Updating modal logic

--- dingdong.js
+++ dingdong.js
@@ -3,57 +3,70 @@
 	function defaultHandler(data, callback) {
 		$.post("/dingdong", data)
 			.done(function () {
-				callback();
-			})
-			.fail(function () {
-				alert("Something went wrong :/");
-				callback("error");
-			});
+		    callback();
+		  })
+		  .fail(function () {
+		    alert("Something went wrong :/");
+		    callback(true);
+		  });
 	}
-	
+
 	$.dingdong = function(handler) {
 
 		$("body").append(
 			$("<button />", { id: "dingdong-button", style: "display: none", text: "Feedback" })
 		);
-		
+
 		$("body").append(
 			$("<div/>", { id: "dingdong", style: "display: none" }).append(
-				$("<div/>", { id: "dingdong-overlay" }),
 				$("<div/>", { id: "dingdong-box" }).append(
 					$("<div/>", { id: "dingdong-close", text: "\u00D7" }),
 					$("<form/>", { id: "dingdong-form" }).append(
-						$("<textarea/>", { name: "message", rows: "5", placeholder: "Send us your feedback or report an issue", required: "required" }),
-						$("<input/>", { name: "email", type: "email", placeholder: "Your email address", required: "required" }),
+						$("<div/>", { "class": "dingdong-row" }).append(
+							$("<input/>", { id: "dingdong-email", name: "email", type: "email", placeholder: "Your email address", required: "required" })
+						),
+						$("<div/>", { "class": "dingdong-row" }).append(
+							$("<textarea/>", { id: "dingdong-message", name: "message", rows: "7", placeholder: "Send us your feedback or report an issue", required: "required" })
+						),
+						$("<div/>", { "class": "dingdong-row" }).append(
+							$("<label/>", { for: "dingdong-subscribe", text: "Subscribe to newsletter" }).append(
+								$("<input/>", { id: "dingdong-subscribe", name: "subscribe", type: "checkbox" })
+							)
+						),
 						$("<button/>", { id: "dingdong-submit", type: "submit", text: "Send Message" })
 					)
 				)
 			)
 		);
-			
-		$("#dingdong-button").click(function () { 
-			$("#dingdong").show(); 
+
+		$("#dingdong-button").click(function () {
+			$("#dingdong").show();
 		});
-		
-		$("#dingdong-overlay, #dingdong-close").click(function () {
+
+		$("#dingdong, #dingdong-close").click(function () {
 			$("#dingdong").hide();
 		});
-		
+
+		$("#dingdong-box").click(function (e) {
+			e.stopPropagation();
+		});
+
 		$("#dingdong-form").submit(function (e) {
 			e.preventDefault();
-			var data = { 
-				message: $("#dingdong-form textarea").val(),
-				email: $("#dingdong-form input").val()
+			var data = {
+				message: $("#dingdong-message").val(),
+				email: $("#dingdong-email").val(),
+				subscribe: $("#dingdong-subscribe").is(":checked")
 			};
 			(handler || defaultHandler)(data, function (err) {
 				if (!err) {
-					$("#dingdong").hide(); 
-					$("#dingdong-form")[0].reset();
+					$("#dingdong").hide();
+					$("#dingdong-message").val("");
 				}
 			});
 			return false;
 		});
-		
+
 	}
-	
+
 }(jQuery));
